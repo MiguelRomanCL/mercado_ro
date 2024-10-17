816c904783fc9a731075c757ab7a553c53d9b19d
Matias <mncarvaj1@gmail.com>
2024-10-17 13:05:21 -0300

    cambiada la logica de la estrategia naive a una simple que solo evalue mav y ultima oferta

--- uso_estrategia_naive.py
+++ uso_estrategia_naive.py
@@ -1,48 +1,42 @@
 # Versión para EDA de la estrategia naive en el pasado
 import estrategia_naive as naive
 import pandas as pd
+import numpy as np
+from scipy import stats
+
+from parameters import (
+    PROP_MIN_MAV_VALID,
+    PROP_MAX_MAV_VALID,
+    PROP_UMBRAL_COMPRA,
+    GANANCIA_BRUTA_MIN,
+    GANANCIA_PORCENTUAL_MIN,
+    MIN_PERIODS_STD,
+    UMBRAL_STD_MULTI,
+    DIAS_MAX_ESPERA,
+    TIEMPO_SLEEP_ENTRE_CARTAS,
+    LAST_VALUES_TO_CONSIDER,
+    GANANCIA_BRUTA_DIARIA 
+)
+
 
 df_cartas = naive.cargar_datos()
 # restriccion temporal para no cargar todas las cartas
 df_cartas = df_cartas.head(3)
 
-df_resultados = naive.analizar_estrategia_naive_todas_las_cartas(df_cartas)
+# Logica para 1 carta
+card_id = df_cartas['ID'][0]
+sells_df, listing_df = naive.obtener_datos_carta(card_id)
+last_values_sold = sells_df.sort_values(by="ds", ascending=False).reset_index(drop=True)[0:LAST_VALUES_TO_CONSIDER]['y']
+removed_extremes = np.sort(last_values_sold)[1:-1] 
+mav = removed_extremes.mean()
+std = removed_extremes.std()
+
+last_date_listed = listing_df.sort_values(by="ds", ascending=False).reset_index(drop=True)['ds'][0]
+cheaper_value = listing_df[listing_df['ds'] == last_date_listed].sort_values(by = 'y',ascending = True).reset_index(drop=True)['y'][0]
+opportunity_by_value = int(cheaper_value < mav - std)
 
 
-## También dejamos otro ejemplo de como analizar la información en tiempo real
-# Versión para tiempo real
-import estrategia_naive as naive
-
-import pandas as pd
-from parameters import TIEMPO_SLEEP_ENTRE_CARTAS
-import time
-from tqdm import tqdm
-
-df_cartas = naive.cargar_datos()
-
-
-# Configurar pandas para usar un formato específico en los números flotantes
-# pd.set_option("display.float_format", "{:,.0f}".format)
-
-df_cartas = df_cartas.head(10)
-
-list_of_dfs = []
-for index, carta in tqdm(df_cartas.iterrows(), total=df_cartas.shape[0]):
-    id_carta = carta["ID"]
-    nombre_carta = carta["Carta"]
-
-    listing_df_enriched = naive.obtener_listing_df_con_info_sells(id_carta)
-    last_date_ts = listing_df_enriched["ds"].max()
-    listing_df_last = listing_df_enriched.query("ds == @last_date_ts").copy()
-    listing_df_last["Carta"] = nombre_carta
-    list_of_dfs.append(listing_df_last)
-    time.sleep(
-            TIEMPO_SLEEP_ENTRE_CARTAS
-        ) 
-
-df = pd.concat(list_of_dfs).reset_index(drop=True)
-
-# TODO: Revisar porqué hay casos con fecha_ultima_venta superior a la fecha actual
-df["Dias_Transcurridos_tras_ultima_venta"] = (
-    df["ds"] - df["fecha_ultima_venta"]
-).dt.days
+mean_days_to_sell = sells_df['ds'].diff().tail(7).reset_index(drop=True).apply(lambda x: float(x.days)).mean()
+ganancia_por_dia = (mav - cheaper_value)/mean_days_to_sell
+opportunity_by_ganancia = int(ganancia_por_dia > GANANCIA_BRUTA_DIARIA)
+opportunity = int(opportunity_by_value + opportunity_by_ganancia == 2)
