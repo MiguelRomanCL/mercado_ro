3fd5d93802c75a15d494663412dd19e7f51f3763
matias carvajal <mncarvaj1@gmail.com>
2024-10-25 04:44:13 +0000

    eliminado una parte que era de debugeo y no sirve realmente para ejecutarlo mensualmente

--- uso_estrategia_naive.py
+++ uso_estrategia_naive.py
@@ -32,25 +32,3 @@ for card_id in df_cartas['ID']:
 df_with_card_names = pd.merge(df_cartas, all_cards_opportunities, how = 'left', on = 'ID')
 df_with_card_names.to_csv('test.csv', sep = ';')
 
-
-###
-
-mav, std, last_date_listed, cheaper_value, last_sell_price, mean_days_to_sell, opportunity = naive.detectar_oportunidad_una_carta(4255)
-
-# Create a temporary DataFrame for the current card
-temp_df = pd.DataFrame({
-    'ID': [4255],
-    'mav': [mav],
-    'std': [std],
-    'last_date_listed': [last_date_listed],
-    'last_sell_price': [last_sell_price],
-    'cheaper_value': [cheaper_value],
-    'mean_days_to_sell': [mean_days_to_sell],
-    'opportunity': [opportunity]
-})
-
-
-sells_df_pupa, listing_df_pupa = naive.obtener_datos_carta(4003)
-sells_df_bonechewer, listing_df_bonechewer= naive.obtener_datos_carta(8238)
-
-df_with_card_names[df_with_card_names['cheaper_value'] < df_with_card_names['mav']]
