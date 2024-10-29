394fb0e3595f1a7e7610d9d69a87baa9a164f988
matias carvajal <mncarvaj1@gmail.com>
2024-10-29 16:33:33 +0000

    agregue para que escriba el archivo resultante a S3 en el formato dado por la estructura de la query de athena del archivo

--- uso_estrategia_naive.py
+++ uso_estrategia_naive.py
@@ -2,6 +2,10 @@
 import estrategia_naive as naive
 import pandas as pd
 import time
+import boto3
+import numpy as np
+from datetime import datetime
+
 from parameters import (
     TIEMPO_SLEEP_ENTRE_CARTAS,
 )
@@ -35,4 +39,43 @@ for card_id in df_cartas['ID']:
     time.sleep(TIEMPO_SLEEP_ENTRE_CARTAS)
 
 df_with_card_names = pd.merge(df_cartas, all_cards_opportunities, how = 'left', on = 'ID')
-df_with_card_names.to_csv('test.csv', sep = ';')
+
+#para poder conectarlo correctamente con la base en athena, debe tener el formato del archivo que esta en athena 
+# (esta parte no esta automática y todo se crea manual igual que los formatos a continuacion)
+
+today_date = datetime.today().strftime('%Y-%m-%d')
+df_with_card_names['extracted_date'] = today_date
+df_with_card_names['mav'] = df_with_card_names['mav'].astype(np.float32)
+df_with_card_names['std'] = df_with_card_names['std'].astype(np.float32)
+df_with_card_names['last_sell_price'] = df_with_card_names['last_sell_price'].astype(np.float32)
+df_with_card_names['cheaper_value'] = df_with_card_names['cheaper_value'].astype(np.float32)
+df_with_card_names['mean_days_to_sell'] = df_with_card_names['mean_days_to_sell'].astype(np.float32)
+
+# Convertir la columna de fecha a solo fecha (sin tiempo)
+df_with_card_names['last_date_listed'] = pd.to_datetime(df_with_card_names['last_date_listed']).dt.date
+df_with_card_names['opportunity'] = df_with_card_names['opportunity'].fillna(-1).astype(np.int32)
+
+df_with_card_names.to_parquet('tmp/tmp.parquet', engine='pyarrow')
+
+
+#esto esta muy feo pero debo salir del paso para leer las credentials
+with open('/home/ubuntu/.aws/credentials') as file:
+    next(file)
+    # Read the next two lines
+    key_id = file.readline().strip()
+    access_key = file.readline().strip()
+    
+    # Get the part after '=' in each line
+    key_id = key_id.split('=', 1)[1].strip()
+    access_key = access_key.split('=', 1)[1].strip()
+
+session = boto3.Session(
+    aws_access_key_id=key_id,
+    aws_secret_access_key=access_key
+)
+
+s3 = session.client('s3')
+bucket_name = 'romillario-opportunities'
+name_folder = 'opportunities_naive'
+s3_file_path = f'{name_folder}/extracted_date={today_date}/df_opportunities_{today_date}.parquet'
+s3.upload_file('tmp/tmp.parquet', bucket_name, s3_file_path)
